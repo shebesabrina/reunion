37bd007c121dabdab36837c6514a9e7e3d5f25ba
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-21 09:23:42 -0700

    Adding the activiyt and the price.

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,10 @@
 class Activity
+  attr_reader :activity,
+              :price
+
+  def initialize(activity, price)
+    @activity= activity
+    @price = price
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,16 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def setup
+    @activity = Activity.new("ski", "$100")
+  end
+
+  def test_it_has_an_activity
+    assert_equal "ski", @activity.activity
+  end
+
+  def test_it_has_a_price
+    assert_equal "$100", @activity.price
+  end
+
 end
